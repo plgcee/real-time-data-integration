4ff3f24918787437ac5c3f22bd8cd38e584a6237
Koushik S <48856232+Kaushiks4@users.noreply.github.com>
2025-04-10 22:27:05 +0530

    Ignore eslint

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Disable ESLint
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   webpack: (config, { dev, isServer }) => {
     if (dev && !isServer) {
       config.optimization.splitChunks = false;
