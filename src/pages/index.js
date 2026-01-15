e029785071fe246cc082461ecd333ac612ade823
rsingal-confluent <rsingal@confluent.io>
2026-01-15 07:22:56 +0530

    Updated index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,16 @@ import RecordList from "@/components/RecordList";
 import CreateSchemaForm from "@/components/CreateSchemaForm";
 import CreateDatabaseForm from "@/components/CreateDatabaseForm";
 import InsertRecordsForm from "@/components/InsertRecordsForm";
+import AnalyticsButton from "@/components/AnalyticsButton";
 
 export default function Home() {
   return (
     <div className="container-sm">
 <h1 className="text-center text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-blue-400 via-purple-500 to-pink-500 drop-shadow-lg m-4">
-  Real-Time Data Integration & Replication</h1>      
+  Real-Time Data Integration & Replication</h1>
+  <div className="flex justify-center">
+        <AnalyticsButton/>
+    </div>   
 <div className="flex">
         <div className="flex-1"><CreateDatabaseForm/></div>
         <div className="flex-2"><CreateSchemaForm/></div>
